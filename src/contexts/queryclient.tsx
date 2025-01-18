b364bd102a47f7517f00ec877222adefa29c9a0d
Alex Veenendaal <alex@opendoorgonorth.com>
2025-01-18 14:22:23 +0000

    chore: set q cache to never timeout

--- src/contexts/queryclient.tsx
+++ src/contexts/queryclient.tsx
@@ -4,7 +4,14 @@ import { ReactNode } from 'react';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      gcTime: Infinity
+    }
+  }
+});
 
 export const QueryClientContextProvider = ({
   children
