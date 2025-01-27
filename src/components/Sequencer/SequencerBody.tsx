f1999c6d393375d95dec0a719b934ed43e742217
Alex Veenendaal <alex@opendoorgonorth.com>
2025-01-27 11:26:48 +0000

    chore: setting up grid cells

--- src/components/Sequencer/SequencerBody.tsx
+++ src/components/Sequencer/SequencerBody.tsx
@@ -1,5 +1,50 @@
 'use client';
 
-export const SequencerBody = () => {
-  return <div className='vo-seq-body bg-slate-900 w-[3000px] h-full'>body</div>;
+import { useCallback, useMemo } from 'react';
+
+export interface SequencerBodyProps {
+  padCount: number;
+}
+
+export const SequencerBody = ({ padCount }: SequencerBodyProps) => {
+  const barCount = 24;
+
+  const cells = useMemo(() => {
+    return Array.from({ length: padCount }, (_, index) => {
+      return row(index, barCount);
+    });
+  }, [barCount, padCount]);
+
+  return (
+    <div className='vo-seq-body w-[3000px] h-full'>
+      <div
+        className='grid grid-cols-2 w-full h-full '
+        style={{
+          gridTemplateColumns: `10px repeat(${barCount}, 40px)`,
+          gridTemplateRows: `1fr repeat(${padCount}, 1fr) 0.2fr`
+        }}
+      >
+        <div className='vo-seq-header  col-span-1'></div>
+        <div className='vo-seq-gutter col-span-1'></div>
+        {cells}
+      </div>
+    </div>
+  );
+};
+
+const row = (rowIndex: number, length: number) => {
+  return Array.from({ length }, (_, index) => {
+    return (
+      <div
+        key={`ch-${index}`}
+        className='text-gray-400 shadow-[inset_0_-2px_4px_rgba(0,0,0,0.6)] w-[40px] text-xs flex justify-center items-center'
+        style={{
+          gridRow: `${rowIndex + 2}/${rowIndex + 2}`,
+          gridColumn: `${index + 2}/${index + 2}`
+        }}
+      >
+        {rowIndex + 1},{index + 1}
+      </div>
+    );
+  });
 };
