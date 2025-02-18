e22e4fd6583b20faacdc98c08309cb9bf0327061
Alex Veenendaal <alex@opendoorgonorth.com>
2025-02-18 11:44:22 +0000

    chore: css layout for stepseq

--- src/components/StepSequencer/index.tsx
+++ src/components/StepSequencer/index.tsx
@@ -4,41 +4,86 @@
 // import { SequencerPad } from './SequencerPad';
 // import { TimeSequencerBody } from './TimeSequencer/TimeSequencerBody';
 
+const CELL_HEIGHT = 60;
+
 export const StepSequencer = () => {
   // const { pads } = usePads();
 
   // const padCount = pads?.length ?? 1;
 
   return (
-    <div className='vo-step-seq flex mt-4 w-full flex-grow bg-c2 rounded-lg'>
-      {/* <div
-        className='grid grid-cols-2 w-full'
+    <div className='w-full h-full mt-4 mb-4 rounded-lg bg-c2 border border-gray-300 flex'>
+      <div
+        className='w-24 grid'
         style={{
-          gridTemplateColumns: `0.05fr 1fr`,
-          gridTemplateRows: `1fr repeat(${padCount}, minmax(0, 1fr)) 0.6fr`
+          gridTemplateRows: `3em repeat(4, ${CELL_HEIGHT}px) 1em`
         }}
       >
-        <div className='vo-seq-corner relative col-span-1 flex justify-center items-center'></div>
-        {pads.map((pad, index) => (
-          <div
-            key={`pad-${pad.id}`}
-            className=''
-            style={{ gridArea: `${index + 2}/1/auto/span 1` }}
-          >
-            <SequencerPad pad={pad} />
-          </div>
-        ))}
-        <div
-          className='vo-seq-footer col-span-2'
-          style={{ gridArea: `${padCount + 2}/1/auto/span 1` }}
-        ></div>
-        <div
-          className='vo-seq-body-wrapper overflow-x-scroll overflow-y-hidden'
-          style={{ gridColumn: `2/3`, gridRow: `1/${padCount + 3}` }}
-        >
-          <TimeSequencerBody pads={pads} />
-        </div>
-      </div> */}
+        <div className='border-b border-gray-300 w-full'></div>
+        <RowHeader index={1} />
+        <RowHeader index={2} />
+        <RowHeader index={3} />
+        <RowHeader index={4} />
+        <div className='h-6 '></div>
+      </div>
+
+      <ScrollableArea />
+    </div>
+  );
+};
+
+const ScrollableArea = () => {
+  return (
+    <div
+      className='grid overflow-x-auto overflow-y-hidden'
+      style={{
+        gridTemplateRows: `3em repeat(4, ${CELL_HEIGHT}px) 1px`,
+        gridTemplateColumns: 'repeat(5, 1fr)',
+        gridAutoFlow: 'row'
+      }}
+    >
+      <div
+        className='header border-l border-b border-gray-300 w-full'
+        style={{
+          gridColumn: '1 / -1'
+        }}
+      ></div>
+      <Cell index={1} />
+      <Cell index={2} />
+      <Cell index={3} />
+      <Cell index={4} />
+      <Cell index={5} />
+      <Cell index={6} />
+      <Cell index={7} />
+      <Cell index={8} />
+      <Cell index={9} />
+      <Cell index={10} />
+      <Cell index={11} />
+      <Cell index={12} />
+      <Cell index={13} />
+      <Cell index={14} />
+      <Cell index={15} />
+      <Cell index={16} />
+      <Cell index={17} />
+      <Cell index={18} />
+      <Cell index={19} />
+      <Cell index={20} />
+    </div>
+  );
+};
+
+const RowHeader = ({ index }: { index: number }) => {
+  return (
+    <div className='border-b border-gray-300 flex items-center justify-center'>
+      Row {index}
+    </div>
+  );
+};
+
+const Cell = ({ index }: { index: number }) => {
+  return (
+    <div className='border-l border-b border-gray-300 w-[200px] flex items-center justify-center'>
+      Cell {index}
     </div>
   );
 };
